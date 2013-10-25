079ad8fad4ae8dbad264e624e5acd4cf95c54afa
Tjdosa <takuma.valcourt@gmail.com>
2013-10-25 12:32:21 -0700

    clean up. Ver. 1.01
    Took out Obfuscated Code

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+<link rel="stylesheet" href="includes/style.css">
+
 <h1>Create A User</h1>
 <form action="create.php" method="post">
 <p>
-  <input type="text" name="inputName" value="" />
+<div id="div_Name">
+<div>
+<div id="div_name_label">
+	First Name:
+</div>
+  <input id="inputName" type="text" name="inputName" value="" />
+</div>
+<div>
+<div id="div_name_label">
+	Last Name:
+</div>
+  <input id="inputName" type="text" name"inputLname" value=""/>
+</div>
+</div>  
   <br />
-  <input name="inputDesc" type="text" value="" width="80%" height="50%" />
+  <input id="inputDesc" name="inputDesc" type="text" value=""  />
 </p>
 <p>
   <input type="submit" name="submit" />
